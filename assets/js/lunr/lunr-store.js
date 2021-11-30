f3cce3ac18c1bb05149939eee18566b7ae904a25
Néstor González <alu0100108859@ull.edu.es>
2021-11-30 16:42:31 +0000

    new-deploy

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Daredevil, la mejor serie de Marvel",
+        "excerpt":"Daredevil, la mejor serie de Marvel Daredevil sorprendió llevando al Universo Cinematográfico de Marvel (MCU) a un terreno nuevo e inexplorado en las películas y series antes mencionadas, con un tono más oscuro y realista, logrando al mismo tiempo una fidelidad al cómic que recompensará a los fanáticos y conocedores...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/post2",
+        "teaser": null
+      },{
+        "title": "Aprendizaje Individual",
+        "excerpt":"En este tipo de aprendizaje cada alumno trabaja de manera independiente para su propio beneficio personal sin apenas contacto con el resto de compañeros. El hecho de que un alumno alcance o no su meta no va a influir en que los demás alcancen o no las suya. Ventajas Desventajas...","categories": ["educacion"],
+        "tags": [],
+        "url": "/educacion/practica3",
+        "teaser": null
+      },{
+        "title": "Aprendizaje Competitivo",
+        "excerpt":"En este tipo de aprendizaje se busca una rivalidad entre alumnos para ver quién aprende más, mejor y más deprisa. Según este modelo, un alumno se encuentra pendiente de dos cosas: la equivocación de un compañero para así obtener ventaja; y, la evaluación positiva de su aprendizaje, una valoración que...","categories": ["educacion"],
+        "tags": [],
+        "url": "/educacion/practica4",
+        "teaser": null
+      },{
+        "title": "Aprendizaje Cooperativo",
+        "excerpt":"En este tipo de aprendizaje consiste en alumnos que trabajan juntos para alcanzar objetivos comunes, procurando obtener resultados que sean beneficiosos para todos los miembros del grupo, maximizando así su propio aprendizaje . Este aprendizaje es grupal y solo puede darse en el pequeño grupo. A diferencia del aprendizaje cooperativo...","categories": ["educacion"],
+        "tags": [],
+        "url": "/educacion/practica5",
+        "teaser": null
+      }]
